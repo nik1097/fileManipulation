a02327fa5e10a3811dccf757983938b0f7fc4aad
Nikhil Suresh <nikhilsuresh097@gmail.com>
2022-10-03 17:55:59 -0400

    rotation completed

--- rotator.cpp
+++ rotator.cpp
@@ -15,37 +15,52 @@ void Rotator::rotate(Pgm* input, int degrees, string output_file){
     int width = input->getWidth(); // numofcols
     int height = input->getHeight(); //numofrows
 
-    vector<vector<GrayPixel*>> rev_array(height, vector<GrayPixel*>(width));
-    
-    //vector<vector<GrayPixel*>> rev_array(width, vector<GrayPixel*>(height));
-
-    //if(degrees == 90)
-        //rev_array.resize(width, vector<GrayPixel*>(height));
-
-    for(int row = 0; row < height; ++row){
-        for (int col = 0; col < width; ++col){
-            if (degrees == 90)
-               rev_array[col][height-row-1] = input->gp_array[row][col]; 
-            else if (degrees == 180)
-                rev_array[height-row-1][width-col-1] = input->gp_array[row][col];
-            else if (degrees == 270)
-                rev_array[width-col-1][row] = input->gp_array[row][col];
-        }
-    } 
-    ofstream myfile(output_file);
-    if (myfile.is_open()){
-        myfile << "P" << magicNum << endl;
-        myfile << "# Rotated image\n";
-        myfile << width<<" "<<height<<endl; 
-        myfile << maxVal<<endl;
+    //vector<vector<GrayPixel*>> rev_array(height, vector<GrayPixel*>(width)); // works for 180 m x n
+    if(degrees == 90 || degrees == 270){
+        vector<vector<GrayPixel*>> rev_array(width, vector<GrayPixel*>(height)); // works for 90 m x n
         for(int row = 0; row < height; ++row){
-            for (int col = 0; col < width; ++col){ 
-                GrayPixel* gp = rev_array[row][col];
-                myfile << gp->getGp() << "\n";
+            for (int col = 0; col < width; ++col){
+                if (degrees == 90)
+                    rev_array[col][height-row-1] = input->gp_array[row][col]; 
+                else if (degrees == 270)
+                    rev_array[width-col-1][row] = input->gp_array[row][col];
+            }
+        } 
+        ofstream myfile(output_file);
+        if (myfile.is_open()){
+            myfile << "P" << magicNum << endl;
+            myfile << "# Rotated image\n";
+            myfile << height<<" "<<width<<endl; 
+            myfile << maxVal<<endl;
+            for(int row = 0; row < width; ++row){
+                for (int col = 0; col < height; ++col){ 
+                    GrayPixel* gp = rev_array[row][col];
+                    myfile << gp->getGp() << "\n";
+                }
             }
         }
+        myfile.close();
     }
-    myfile.close();
+    else if (degrees == 180){
+        vector<vector<GrayPixel*>> rev_array(height, vector<GrayPixel*>(width));
+        for(int row = 0; row < height; ++row)
+            for (int col = 0; col < width; ++col)
+                rev_array[height-row-1][width-col-1] = input->gp_array[row][col];
+        ofstream myfile(output_file);
+        if (myfile.is_open()){
+            myfile << "P" << magicNum << endl;
+            myfile << "# Rotated image\n";
+            myfile << width<<" "<<height<<endl; 
+            myfile << maxVal<<endl;
+            for(int row = 0; row < height; ++row){
+                for (int col = 0; col < width; ++col){ 
+                    GrayPixel* gp = rev_array[row][col];
+                    myfile << gp->getGp() << "\n";
+                }
+            }
+        }
+        myfile.close();        
+    }   
 }
 
 void Rotator::rotate(Ppm* input, int degrees, string output_file){
@@ -54,34 +69,50 @@ void Rotator::rotate(Ppm* input, int degrees, string output_file){
     int width = input->getWidth(); // numofcols
     int height = input->getHeight(); //numofrows
 
-    vector<vector<ColorPixel*>> rev_array(height, vector<ColorPixel*>(width));
-    //vector<vector<GrayPixel*>> rev_array(width, vector<GrayPixel*>(height));
-
-    //if(degrees == 90)
-        //rev_array.resize(width, vector<GrayPixel*>(height));
-
-    for(int row = 0; row < height; ++row){
-        for (int col = 0; col < width; ++col){
-            if (degrees == 90)
-               rev_array[col][height-row-1] = input->cp_array[row][col]; 
-            else if (degrees == 180)
-                rev_array[height-row-1][width-col-1] = input->cp_array[row][col];
-            else if (degrees == 270)
-                rev_array[width-col-1][row] = input->cp_array[row][col];
-        }
-    } 
-    ofstream myfile(output_file);
-    if (myfile.is_open()){
-        myfile << "P" << magicNum << endl;
-        myfile << "# Rotated image\n";
-        myfile << width<<" "<<height<<endl; 
-        myfile << maxVal<<endl;
+    if(degrees == 90 || degrees == 270){
+        vector<vector<ColorPixel*>> rev_array(width, vector<ColorPixel*>(height));
         for(int row = 0; row < height; ++row){
-            for (int col = 0; col < width; ++col){ 
-                ColorPixel* cp = rev_array[row][col];
-                myfile << cp->getRed() <<" "<< cp->getGreen() <<" "<< cp->getBlue() <<"\n";
+            for (int col = 0; col < width; ++col){
+                if (degrees == 90)
+                    rev_array[col][height-row-1] = input->cp_array[row][col]; 
+                else if (degrees == 270)
+                    rev_array[width-col-1][row] = input->cp_array[row][col];
+            }
+        } 
+        ofstream myfile(output_file);
+        if (myfile.is_open()){
+            myfile << "P" << magicNum << endl;
+            myfile << "# Rotated image\n";
+            myfile << height<<" "<<width<<endl; 
+            myfile << maxVal<<endl;
+            for(int row = 0; row < height; ++row){
+                for (int col = 0; col < width; ++col){ 
+                    ColorPixel* cp = rev_array[row][col];
+                    myfile << cp->getRed() <<" "<< cp->getGreen() <<" "<< cp->getBlue() <<"\n";
+                }
             }
         }
+        myfile.close();
     }
-    myfile.close();
+    else if(degrees = 180){
+        vector<vector<ColorPixel*>> rev_array(height, vector<ColorPixel*>(width));
+        for(int row = 0; row < height; ++row)
+            for (int col = 0; col < width; ++col)
+                rev_array[height-row-1][width-col-1] = input->cp_array[row][col];
+                
+        ofstream myfile(output_file);
+        if (myfile.is_open()){
+            myfile << "P" << magicNum << endl;
+            myfile << "# Rotated image\n";
+            myfile << width<<" "<<height<<endl; 
+            myfile << maxVal<<endl;
+            for(int row = 0; row < height; ++row){
+                for (int col = 0; col < width; ++col){ 
+                    ColorPixel* cp = rev_array[row][col];
+                    myfile << cp->getRed() <<" "<< cp->getGreen() <<" "<< cp->getBlue() <<"\n";
+                }
+            }
+        }
+        myfile.close();
+    }
 }
